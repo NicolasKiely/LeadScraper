f17a30d85d75a5aaf2054d3c2516e2fcfa487e9e
Nicolas Kiely <mitchell3141@gmail.com>
2018-12-16 20:10:28 -0500

    Overall scraper logic

--- scraper.py
+++ scraper.py
@@ -1,1 +1,15 @@
-""" Pull in webpages from pubmed and save them to database """
+""" Pull in webpages from pubmed and save them to database
+
+Foreach pmid entry in db:
+    Is article in local archive?
+        If not, download from pubmed
+    Get authors and links from page
+    Does article have links to original journal?
+        If so, go to sourcing journal
+        Is source journal article in archive?
+            If not, download from source journal
+        Get authors and links from source journal
+    Save authors for journal, prioritizing original journal
+"""
+
+print('Running')
